a90f8f42c44386b6a66211e51873593cffbdff8d
梦未 <159781497+wytx0710@users.noreply.github.com>
2024-06-22 15:00:10 +0800

    Update works.js

--- works.js
+++ works.js
@@ -1,56 +1,14 @@
 
 // 定义存放图片链接的数组
 const imageGroups = {
-  'background_mobile': [
-  'https://github.com/shiraayano/img-api/raw/main/background_mobile/104531347_p0.jpg',
-  'https://github.com/shiraayano/img-api/raw/main/background_mobile/111058829_p0.jpg',
-  'https://github.com/shiraayano/img-api/raw/main/background_mobile/111635936_p0.jpg',
-  'https://github.com/shiraayano/img-api/raw/main/background_mobile/71684890_p0.jpg',
-  'https://github.com/shiraayano/img-api/raw/main/background_mobile/82869627_p0.jpg',
+  'mobile': [
+ 
   ],
-  'background_tablet': [
-  'https://github.com/shiraayano/img-api/raw/main/background_tablet/109838747_p0.jpg',
-  'https://github.com/shiraayano/img-api/raw/main/background_tablet/111317868_p0.jpg',
-  'https://github.com/shiraayano/img-api/raw/main/background_tablet/112052605_p0.jpg',
-  'https://github.com/shiraayano/img-api/raw/main/background_tablet/67638415_p0.jpg',
-  'https://github.com/shiraayano/img-api/raw/main/background_tablet/70844305_p0.jpg',
-  'https://github.com/shiraayano/img-api/raw/main/background_tablet/96781044_p0.jpg',
-  'https://github.com/shiraayano/img-api/raw/main/background_tablet/99142869_p0.jpg',
+  'tablet': [
+ 
   ],
-  'background_pc': [
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/100783147_p0.png',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/109884134_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/28827480_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/57221.png',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/63936697_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/66589938_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/70266119_p0.png',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/71566096_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/75487936_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/82720559_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/82782742_p0.png',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/82910218_p0.png',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/82924488_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/83404272_p0.png',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/85603687_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/87617434_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/3/91192019_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/2/109034541_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/2/109213534_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/2/109509425_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/2/58733791_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/2/70626212_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/1/102996140_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/1/66343936_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/1/67280596_p0.png',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/1/67506480_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/1/78104901_p0.png',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/1/80468767_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/1/81731861_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/1/85819594_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/1/89893271_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/1/90945887_p0.jpg',
-  'https://github.com/shiraayano/ChangeWallpaper/raw/main/img/1/94755985_p0.png'
+  'pc': [
+ 
   ]
 };
 
